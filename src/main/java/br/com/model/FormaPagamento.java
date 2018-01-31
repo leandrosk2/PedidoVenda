aff8c62f353dc0a3c6b7864d767f11b6545a123b
leandroferreiralima <leandroferreiralima@192.168.0.104>
2018-01-30 23:55:46 -0200

    Commit aula 66

---
+++ src/main/java/br/com/model/FormaPagamento.java
@@ -0,0 +1,8 @@
+package br.com.model;
+
+public enum FormaPagamento {
+
+	DINHEIRO, CARTAO_CREDITO, CARTAO_DEBITO, 
+	CHEQUE, BOLETO_BANCARIO, DEPOSITO_BANCARIO
+	
+}
--- src/main/java/br/com/service/CalculadoraPreco.java
+++
@@ -1,9 +0,0 @@
-package br.com.service;
-
-public class CalculadoraPreco {
-
-	public double calcularPreco(int quantidade, double precoUnitario) {
-		return quantidade * precoUnitario;
-	}
-	
-}
